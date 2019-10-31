308315dbb558f7df5d892c1f06e19b9bc58e6340
mattcarp12 <mattcarp88@gmail.com>
2019-10-31 06:38:41 -0400

    update

--- src/main/java/org/matt/redisdemo/RedisDemoApplication.java
+++ src/main/java/org/matt/redisdemo/RedisDemoApplication.java
@@ -1,10 +1,14 @@
 package org.matt.redisdemo;
 
+import java.net.URI;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.data.redis.listener.ChannelTopic;
@@ -13,6 +17,9 @@ import org.springframework.data.redis.listener.adapter.MessageListenerAdapter;
 
 @SpringBootApplication
 public class RedisDemoApplication {
+	
+	@Value("${spring.redis.url}")
+	URI redisURI;
 
 	public static void main(String[] args) {
 		ApplicationContext ctx = SpringApplication.run(RedisDemoApplication.class, args);
@@ -51,6 +58,12 @@ public class RedisDemoApplication {
 	
 	@Bean
     public RedisConnectionFactory jedisConnectionFactory(){
-       return new JedisConnectionFactory();
+		RedisStandaloneConfiguration config = new RedisStandaloneConfiguration();
+		config.setHostName(redisURI.getHost());
+		config.setPort(redisURI.getPort());
+		if (!redisURI.getHost().equals("localhost")) config.setPassword(redisURI.getUserInfo().substring(2));
+        JedisConnectionFactory jedisConnectionFactory = new JedisConnectionFactory(config);
+        return jedisConnectionFactory;
     }
+
 }
